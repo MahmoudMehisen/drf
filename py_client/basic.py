43b80361b299227e0f4fc49241e24413de49e704
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-08-25 08:02:42 +0300

    inti project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,9 @@
 import requests
 
-endpoint = "https://www.github.com"
+endpoint = "http://localhost:8000"
+
+get_response = requests.get(endpoint, json={"query": "Hello World"})
+
+print(get_response)
+# print(get_response.text)
+# print(get_response.status_code)
