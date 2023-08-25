d489be0d6866b45cf9e681f74259ec218d775efe
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-08-25 08:19:48 +0300

    add echo get api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,16 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"meessage": "Hi there"})
+
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
