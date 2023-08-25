2419b95a4e347bccd8ae3eff5353e6fddbd8c28c
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-08-26 02:24:19 +0300

    add rest view

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -9,12 +9,16 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            'title',
-            'content',
-            'price',
-            'sale_price',
-            'my_discount',
+            "title",
+            "content",
+            "price",
+            "sale_price",
+            "my_discount",
         ]
 
     def get_my_discount(self, obj):
+        if not hasattr(obj, "id"):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
